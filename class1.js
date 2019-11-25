375be80fb7824aa1559d303558b87975a251414e
ShankyJS <jhancarlossilva.c@gmail.com>
2019-11-24 23:35:00 -0600

    Class 2 added.

--- class1.js
+++ class1.js
@@ -1,6 +1,26 @@
-var nombre = 'Jhan';
-var apellido = 'Silva';
-var edad = 28
-edad = '28 anos'
-console.log('Hola, ' + nombre + ' ' + apellido);
-console.log ('Tengo ' + edad + ' años'); 
+var nombre = 'Jhan', apellido = 'Silva';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoenMinusculas = apellido.toLowerCase();
+
+
+//Seleccionar la primera letra del nombre, segùn su posiciòn.
+
+var primeraLetraDelNombre = nombre.charAt(0);
+
+// 
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+// var edad = 28
+
+// edad = '28 anos'
+
+// //We really don't know the type of a var. 
+
+// var peso = 75;
+
+// console.log('Hola, ' + nombre + ' ' + apellido);
+// console.log ('Tengo ' + edad + ' años'); 
+
+
